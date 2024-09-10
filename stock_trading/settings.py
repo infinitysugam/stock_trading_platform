b4536993843ac28ca584944286e7919453ebc844
infinitysugam <mishrasugam11@gmail.com>
2024-09-10 11:58:50 -0500

    Admin Page

--- stock_trading/settings.py
+++ stock_trading/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'stock_trading.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stock_trading',
+        'USER':'root',
+        'PASSWORD':'root',
+        'HOST':'127.0.0.1',
+        'PORT':'3306'
     }
 }
 
