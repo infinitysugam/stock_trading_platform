3c09e6e82d1c5313caf8ad239f5bb748af3620e0
Nikhil <chaudharyn582@gmail.com>
2024-11-26 20:58:25 -0600

    Login,signup,profile page

--- stock_trading/settings.py
+++ stock_trading/settings.py
@@ -132,3 +132,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = '/'  # Redirect to the homepage or desired URL
+
--- stock_trading/urls.py
+++ stock_trading/urls.py
@@ -27,7 +27,8 @@ urlpatterns = [
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('logout/',auth_views.LogoutView.as_view(template_name='logout.html'),name='logout'),
     path('register/',user_views.register,name='register'),
-    path('order_management/',order_views.home,name='order_management')
+    path('order_management/',order_views.home,name='order_management'),
+    path('profile/',user_views.profile,name='profile')
 
 
 ]
